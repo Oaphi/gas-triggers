11dccf6c35e8eb5d960442a809c7d0f8c67f4182
Oleg Valter <o.a.philippov@gmail.com>
2020-09-19 18:13:38 +0300

    Fixed bug with dayly trigger start time and added inRnage utility

--- src/clock.js
+++ src/clock.js
@@ -1,29 +1,10 @@
-/**
- * @typedef {{
- *  at : (number|string|Date|undefined),
- *  days : (number|undefined),
- *  hourly : (number|undefined),
- *  minutely : (number|undefined),
- *  weeks : (number|undefined),
- *  weekDay : (GoogleAppsScript.Base.Weekday|undefined),
- *  timezone : string
- * }} TimedTriggerInstallConfig
- * 
- * @param {TimedTriggerInstallConfig} 
- */
 const timedTriggerInstaller = ({
-    at,
+    at, atHour,
     minutely, hourly,
     days, weeks, weekDay,
     timezone = Session.getScriptTimeZone()
 }) =>
 
-    /**
-     * @param {{
-     *  callbackName : string
-     * }}
-     * @returns {GoogleAppsScript.Script.Trigger}
-     */
     ({ callbackName }) => {
 
         try {
@@ -41,25 +22,25 @@ const timedTriggerInstaller = ({
                 builder.everyMinutes(validatedMinutely);
             }
 
+            if (weeks) {
+                builder.everyWeeks(weeks).onWeekDay(weekDay || ScriptApp.WeekDay.SUNDAY);
+            }
+
+            if (days && !weeks) {
+                const atDate = new Date(at !== void 0 ? at : Date.now());
+                builder.everyDays(days).atHour(atHour !== void 0 ? atHour : atDate.getHours());
+            }
+
             if (hourly && !at && !minutely) {
                 builder.everyHours(hourly);
             }
 
-            if (!hourly && at && !minutely) {
+            if (!hourly && !days && at && !minutely) {
                 builder.at(new Date(at));
-            }
-
-            if (days && !weeks) {
-                const atDate = new Date(at || Date.now());
-                builder.everyDays(days).atHour(atDate.getHours());
             }
 
             if (weekDay && !weeks) {
                 builder.onWeekDay(weekDay);
-            }
-
-            if (weeks) {
-                builder.everyWeeks(weeks).onWeekDay(weekDay || ScriptApp.WeekDay.SUNDAY);
             }
 
             builder.inTimezone(timezone);
@@ -71,3 +52,16 @@ const timedTriggerInstaller = ({
         }
 
     };
+
+/**
+ * @type {GoogleAppsScript.Triggers.isInHourlyRange}
+ */
+var isInHourlyRange = ({
+    hour = new Date().getHours(),
+    start = 0,
+    end = 23
+} = {}) => {
+    const validEnd = end > 23 ? 23 : end < 0 ? 0 : end;
+    const validStart = start > 23 ? 23 : start < 0 ? 0 : start;
+    hour >= validEnd && hour <= validStart;
+};
