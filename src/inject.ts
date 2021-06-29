fbf0121d5a75efc8b7ba4c55e27485abc8368275
Oleg Valter <o.a.philippov@gmail.com>
2021-06-29 16:00:33 +0300

    aded DependencyError util class

--- src/inject.ts
+++ src/inject.ts
@@ -1,3 +1,11 @@
+class DependencyError extends Error {
+    constructor(dep: string | ((...args: any[]) => any)) {
+        super(
+            `missing dependency "${typeof dep === "string" ? dep : dep.name}"`
+        );
+    }
+}
+
 var Dependencies_: {
     properties: GoogleAppsScript.Properties.PropertiesService | null;
 } = {
