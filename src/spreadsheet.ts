086bdc8201b5220744cfb78523f36c3b9c729587
Oleg Valter <o.a.philippov@gmail.com>
2021-06-29 16:21:11 +0300

    moved spreadsheet module to TypeScript

---
+++ src/spreadsheet.ts
@@ -0,0 +1,73 @@
+type CommonSheetsInstallerOptions = {
+    spreadsheet?: GoogleAppsScript.Spreadsheet.Spreadsheet;
+};
+
+type EditInstallerOptions = ErrLoggable<CommonSheetsInstallerOptions>;
+
+type ChangeInstallerOptions = ErrLoggable<CommonSheetsInstallerOptions>;
+
+type FormSubmitInstallerOptions = ErrLoggable<CommonSheetsInstallerOptions>;
+
+type InstallerOptions = {
+    callbackName: string;
+};
+
+/**
+ * @summary default spreadsheet onEdit installer
+ */
+const editTriggerInstaller =
+    ({
+        spreadsheet = SpreadsheetApp.getActiveSpreadsheet(),
+        onError = (err) => console.warn(err),
+    }: EditInstallerOptions = {}) =>
+    ({ callbackName }: InstallerOptions) => {
+        try {
+            return ScriptApp.newTrigger(callbackName)
+                .forSpreadsheet(spreadsheet)
+                .onEdit()
+                .create();
+        } catch (error) {
+            onError(`failed to install edit trigger: ${error}`);
+            return null;
+        }
+    };
+
+/**
+ * @summary default spreadsheet onChange installer
+ */
+const changeTriggerInstaller =
+    ({
+        spreadsheet = SpreadsheetApp.getActiveSpreadsheet(),
+        onError = (err) => console.warn(err),
+    }: ChangeInstallerOptions = {}) =>
+    ({ callbackName }: InstallerOptions) => {
+        try {
+            return ScriptApp.newTrigger(callbackName)
+                .forSpreadsheet(spreadsheet)
+                .onChange()
+                .create();
+        } catch (error) {
+            onError(`failed to install change trigger: ${error}`);
+            return null;
+        }
+    };
+
+/**
+ * @summary default spreadsheet onFormSubmit installer
+ */
+const formSubmitTriggerInstaller =
+    ({
+        spreadsheet = SpreadsheetApp.getActiveSpreadsheet(),
+        onError = (err) => console.warn(err),
+    }: FormSubmitInstallerOptions = {}) =>
+    ({ callbackName }: InstallerOptions) => {
+        try {
+            return ScriptApp.newTrigger(callbackName)
+                .forSpreadsheet(spreadsheet)
+                .onFormSubmit()
+                .create();
+        } catch (error) {
+            onError(`failed to install form submit trigger: ${error}`);
+            return null;
+        }
+    };
